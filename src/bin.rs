08d798bc1d6f185d2eb577f24346386590f45e1c
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-06-06 14:58:03 -0400

    Implemented basic fn functionality

--- src/bin.rs
+++ src/bin.rs
@@ -1,39 +1,53 @@
 use any_rust::Datum as any;
-
+use any_rust::FunctionType;
 
 fn main() {
 
-    // let c = any::collection(vec![
-    //         any::string("five"),
-    //         any::number(123),
-    //         any::function(|d| {
-    //             d
-    //         }),
-    //     ]);
+    let f: FunctionType = any::function(|s| s).into();
+    println!("f: {}", (*f)(any::number(5)));
+    println!("f: {}", (*f)(any::string("test")));
+    println!("f: {}", (*f)(any::table()));
 
-    // println!("collection: {}", c.clone());
-    // println!("index: {}", c.clone()[any::number(2)]);
 
-    // let n = any::number(5.1);
-    // println!(
-    //     "int: '{}'\nfloat: '{}'",
-    //     i32::from(n.clone()),
-    //     f64::from(n.clone()),
-    // );
+    let c = any::collection(vec![
+            any::string("five"),
+            any::number(123),
+            any::function(|d| {
+                d
+            }),
+        ]);
+
+    println!("collection: {}", c.clone());
+    println!("index: {}", c.clone()[any::number(2)]);
+
+    let n = any::number(5.1);
+    println!(
+        "int: '{}'\nfloat: '{}'",
+        i32::from(n.clone()),
+        f64::from(n.clone()),
+    );
 
     let mut a = any::table();
     a[any::string("five")] = any::number(7);
-    a = any::number(5);
 
     let mut b = any::table();
-    b[any::string("test")]
-        [any::string("whoa")] = a;
+    b[any::string("test")][any::string("whoa")] = a;
+    a = any::number(5);
+    println!("a = {}", a);
+
+
+    println!("test = {}", b[any::string("whoa_this_is_wrong")]);
+    b[any::string("whoa_this_is_wrong")] = any::number(19.5);
+    println!("test = {}", b[any::string("whoa_this_is_wrong")]);
 
     println!(
         "b/test/whoa = {}",
-        b[any::string("test")]
-            [any::string("whoa")]
+        b[any::string("test")][any::string("whoa")]
     );
+
+
+    let s: String = any::number(5.000000001).to_string();
+    println!("out: {}", s);
 
     // let f = |t: &Table| {
     //     println!("base_table/test = {}", *t.get("test"));
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod any;
-pub use any::{Datum};
+pub use any::*;
 
 mod any_traits;
 pub use any_traits::*;
